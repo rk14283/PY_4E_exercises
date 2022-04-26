e88e0879ff081051e1cab82cda1ff15553346e7c
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-26 19:50:05 +0530

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,3 +1,13 @@
+# Exercise 2.3 
+
+# This first line is provided for you
+
+hrs = float (input("Enter Hours:"))
+rate =float (input("Enter Rate per Hour:"))
+
+pay = float (hrs*rate)
+
+print("Pay: "+ str(pay)) 
 
 
 #Exercise 3.1 
