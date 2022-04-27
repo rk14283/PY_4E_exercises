3e1c75ea19408b59e666940822308edc1c363cc9
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-27 20:33:22 +0530

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -72,6 +72,69 @@ for i in [5,4,3,2,1]:
     print(i)
 print('Blastoff!')
 """      
+
+#loop patterns       
+      
+      
+#Counting in a loop
+zork = 0
+print('Before', zork)
+for thing in [9,41,12,3,74,15]:
+    zork - zork+1
+    print(zork, thing)
+print('After', zork)
+
+
+print
+print
+
+#Summing in a loop 
+zork = 0
+print('Before', zork)
+for thing in [9,41,12,3,74,15]:
+    zork= zork+thing 
+    print(zork,thing)
+print('After', zork)
+
+
+print 
+print 
+
+#Average 
+"""The proces is similar to above but we divide those"""
+
+count = 0
+sum = 0
+print('Before', count, sum)
+for value in [9,41,12,3,74,15]:
+    count = count+1
+    sum = sum + value 
+    print(count, sum, value)
+print('After', count, sum, sum/count)
+
+
+#pattern of something in the begining, something in middle, something in end 
+
+
+print 
+print
+
+"""Filtering in a loop, here we are looking for something
+this may be important for some search algorithm 
+"""
+
+""" we use if statment in the loop to catch/filter the
+values we are looking for 
+"""
+
+print('Before')
+for value in [9,41,12,3,74,15]:
+    if value>20:
+        print('Large number', value)
+print('After')
+
+
+
       
       
       
