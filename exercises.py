94abd2ca8c0081776281c1b7a36714be45501953
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-27 22:33:21 +0530

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -182,6 +182,19 @@ print('After', smallest )
 
       
 #Bear in mind these codes are complicated      
-      
+
+#is and is not, same result as abvoe  
+
+smallest = None
+print('Before')
+for value in [3,41,12,9,74,15]:
+    if smallest is None:
+        smallest = value 
+    elif value<smallest:
+        smallest = value 
+    print smallest, value 
+    
+    
+print ('After', smallest)      
       
       
