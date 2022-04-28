7fcf03b53652a35abe5248c32743a63582ba2b87
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-28 19:23:49 +0530

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -205,3 +205,32 @@ print(tot)
 
 #basically in the above i switches from 0 to 5
       
+      
+#loops are hard 
+      
+"""5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'.
+Once 'done' is entered, print out the largest and smallest of the numbers. 
+If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
+Enter 7, 2, bob, 10, and 4 and match the output below.""" 
+      
+      
+#loops and Iterations
+largest = None
+smallest = None
+while True :
+    sval = input('Enter a number: ')
+    if sval == 'done' :
+        break
+    try:
+        ival = int(sval)
+    except :
+        print('Invalid input')
+    if largest is None or ival > largest :
+        largest = ival
+    elif smallest is None or ival < smallest :
+        smallest = ival
+
+
+print("Maximum is", largest)
+print("Minimum is", smallest)      
+      
