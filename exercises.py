1a28dbe9d0a606552a0a78a08881e39711e61faa
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-04-29 16:22:34 +0530

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -232,5 +232,19 @@ while True :
 
 
 print("Maximum is", largest)
-print("Minimum is", smallest)      
+print("Minimum is", smallest)
+      
+      
+      
+"""6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. 
+      Convert the extracted value to a floating point number and print it out."""
+      
+text = "X-DSPAM-Confidence:    0.8475"
+
+text.find(":")
+
+x = text[23:] 
+
+print (float(x))
+      
       
