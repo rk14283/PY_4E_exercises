186e1f3b632007c21f127b360c4f84f7559b8c92
rk14283 <59180436+rk14283@users.noreply.github.com>
2022-05-03 20:24:46 +0530

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -295,3 +295,30 @@ for word in words:
 romeo.sort()
 print(romeo)
       
+
+"""      
+8.5 Open the file mbox-short.txt and read it line by line.
+When you find a line that starts with 'From ' like the following line:
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). 
+Then print out a count at the end.
+Hint: make sure not to include the lines that start with 'From:'.
+
+
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for l in fh:
+    if not l.startswith("From"):
+        continue
+    else:
+        a=l.split()
+        if len(l.split()) == 7:
+            print(a[1])
+            count+=1
+print("There were", count, "lines in the file with From as the first word")
+Also look at the last line of the sample output to see how to print the count.      
+"""
